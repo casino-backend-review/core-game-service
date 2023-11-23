e8b717e3884e3483ddbfec4361a17f707cc6f284
Leo <Leo.c@sigmaonetechnologies.com>
2023-11-23 22:55:28 +0530

    changes

--- src/main/java/com/core/gameservice/GameServiceApplication.java
+++ src/main/java/com/core/gameservice/GameServiceApplication.java
@@ -2,6 +2,8 @@ package com.core.gameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GameServiceApplication {
@@ -10,4 +12,8 @@ public class GameServiceApplication {
 		SpringApplication.run(GameServiceApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate gRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/core/gameservice/controllers/PgController.java
+++ src/main/java/com/core/gameservice/controllers/PgController.java
@@ -4,42 +4,41 @@ import com.core.gameservice.dto.GameListResponse;
 import com.core.gameservice.dto.PartnerLoginResponse;
 import com.core.gameservice.dto.PgLoginRequest;
 import com.core.gameservice.services.PgService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pg")
 public class PgController {
 
     private final PgService pgService;
+    private static final Logger logger = LoggerFactory.getLogger(PgController.class);
 
-    @Autowired
     public PgController(PgService pgService) {
         this.pgService = pgService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody PgLoginRequest pgLoginRequest) {
+    public ResponseEntity<PartnerLoginResponse> login(@RequestBody PgLoginRequest pgLoginRequest) {
         try {
             PartnerLoginResponse response = pgService.login(pgLoginRequest);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            // Generic exception handling, could be more specific depending on your needs
-            return ResponseEntity.internalServerError().body("An unexpected error occurred");
+            logger.error("Error during login: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().body(new PartnerLoginResponse("Error", e.getMessage()));
         }
     }
 
     @GetMapping("/games")
-    public ResponseEntity<?> gameList() {
+    public ResponseEntity<GameListResponse> gameList() {
         try {
-            GameListResponse response = (GameListResponse) pgService.gameList();
+            GameListResponse response = pgService.gameList();
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("An unexpected error occurred");
+            logger.error("Error fetching game list: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().body(new GameListResponse("Error", e.getMessage()));
         }
     }
 }
